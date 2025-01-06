d13d3ddd18244e66863df5a39894af19ef015855
obadaDeg <animayoloteer@gmail.com>
2025-01-06 16:07:03 +0200

    navbar is almost done

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { useState } from "react";
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import { ThemeProvider } from "@emotion/react";
+import { darkTheme, lightTheme } from "./themes";
 
 function App() {
+  // const theme = 'light'
+
   return (
     <>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={lightTheme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,49 +1,146 @@
 import { NavLink } from "react-router-dom";
 import styles from "./NavBar.module.css";
-import { Button } from "@mui/material";
+import { alpha, Button, useTheme } from "@mui/material";
 import AppLogo from "../../assets/AppLogo";
 import { Home } from "@mui/icons-material";
-import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
-import BedtimeOutlinedIcon from '@mui/icons-material/BedtimeOutlined';
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import BedtimeOutlinedIcon from "@mui/icons-material/BedtimeOutlined";
 
 export default function Navbar() {
+  const theme = useTheme();
+  const secondaryWithOpacity = alpha(theme.palette.text.secondary, 0.05);
+
   return (
-    <>
-      <nav className={styles.sideNavbar}>
+    <nav
+      className={styles.sideNavbar}
+      style={{ backgroundColor: secondaryWithOpacity }}
+    >
+      <header className={styles.navHeader}>
         <AppLogo />
-        <menu className={styles.list}>
-          <li>
+        <h1 className={styles.logoText}>HRMS</h1>
+      </header>
+
+      <menu className={styles.navMenu}>
+        <li>
+          <NavLink
+            to={"/"}
+            className={({ isActive }) =>
+              `${styles.navLink} ${isActive ? styles.active : ""}`
+            }
+            end
+          >
+            <Home className={styles.icon} />
+            Dashboard
+          </NavLink>
+        </li>
+        <li>
             <NavLink
-              to={"/"}
-              className={({ isActive }) => (isActive ? styles.active : "")}
-              end
+                to={"/employees"}
+                className={({ isActive }) =>
+                `${styles.navLink} ${isActive ? styles.active : ""}`
+                }
             >
-              {({ isActive }) => (
-                <>
-                  {isActive ? <Home /> : <Home />}
-                  Dashboard
-                </>
-              )}
+                <Home className={styles.icon} />
+                Employees
             </NavLink>
-          </li>
-        </menu>
+        </li>
+        <li>
+            <NavLink
+                to={"/departments"}
+                className={({ isActive }) =>
+                `${styles.navLink} ${isActive ? styles.active : ""}`
+                }
+            >
+                <Home className={styles.icon} />
+                Departments
+            </NavLink>
+        </li>
+        <li>
+            <NavLink
+                to={"/attendance"}
+                className={({ isActive }) =>
+                `${styles.navLink} ${isActive ? styles.active : ""}`
+                }
+            >
+                <Home className={styles.icon} />
+                Attendance
+            </NavLink>
+        </li>
+        <li>
+            <NavLink
+                to={"/payroll"}
+                className={({ isActive }) =>
+                `${styles.navLink} ${isActive ? styles.active : ""}`
+                }
+            >
+                <Home className={styles.icon} />
+                Payroll
+            </NavLink>
+        </li>
+        <li>
+            <NavLink
+                to={"/jobs"}
+                className={({ isActive }) =>
+                `${styles.navLink} ${isActive ? styles.active : ""}`
+                }
+            >
+                <Home className={styles.icon} />
+                Jobs
+            </NavLink>
+        </li>
+        <li>
+            <NavLink
+                to={"/reports"}
+                className={({ isActive }) =>
+                `${styles.navLink} ${isActive ? styles.active : ""}`
+                }
+            >
+                <Home className={styles.icon} />
+                candidates
+            </NavLink>
+        </li>
+        <li>
+            <NavLink
+                to={"/holidays"}
+                className={({ isActive }) =>
+                `${styles.navLink} ${isActive ? styles.active : ""}`
+                }
+            >
+                <Home className={styles.icon} />
+                Holidays
+            </NavLink>
+        </li>
+        <li>
+            <NavLink
+                to={"/settings"}
+                className={({ isActive }) =>
+                `${styles.navLink} ${isActive ? styles.active : ""}`
+                }
+            >
+                <Home className={styles.icon} />
+                Settings
+            </NavLink>
+        </li>
+      </menu>
+
+      <div className={styles.themeToggle}>
         <Button
           variant="contained"
           color="primary"
-          className={styles.logoutButton}
+          className={styles.toggleButton}
         >
-            <WbSunnyOutlinedIcon />
-            Light
+          <WbSunnyOutlinedIcon />
+          Light
         </Button>
         <Button
           variant="contained"
           color="primary"
-          className={styles.logoutButton}
+          className={styles.toggleButton}
         >
-            <BedtimeOutlinedIcon />
-            Dark
+          <BedtimeOutlinedIcon />
+          Dark
         </Button>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
